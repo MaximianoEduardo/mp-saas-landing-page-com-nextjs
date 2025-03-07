9806736cbe0e6c8ab471289985a934cce23358d1
emaximiano <edu.maxi.96@gmail.com>
2025-03-07 10:36:38 -0300

    feat: Structure for Page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,17 +1,62 @@
 import CallOutComponent from "./components/callout/callout";
+import CardComponent from "./components/card/card";
+import FooterComponent from "./components/footer/footer";
+import EmailFormComponent from "./components/form/form";
 import HeaderComponent from "./components/header/header";
+import HowWorksComponent from "./components/howWorks/howWorks";
+import { Button } from "./components/ui/button";
+
 
 export default function Home() {
   return (
-    <main className="max-w-[1352px] mx-auto">
-      <header>
+    <main className="mx-auto w-full">
+      <header className="lg:max-w-[1352px] max-w-full mx-auto">
 
         <HeaderComponent />
       </header>
 
 
-      <section>
-        <CallOutComponent />
+      <section className="px-4 md:px-0">
+        <CallOutComponent
+            isH1={true}
+            title="Simplifique Seus Estudos" 
+            description="Deixe que nós fazemos a curadoria para você. Assine nossa plataforma e receba todos os meses um ebook novo de programação.">
+          <EmailFormComponent />
+        </CallOutComponent>
+      </section>
+
+
+      <section className="w-full bg-white px-4 md:px-0">
+        <HowWorksComponent />
+      </section>
+
+      <section className="px-4 md:px-0">
+        <CallOutComponent
+            title="Preço Simples e Transparente" 
+            description="Pra que inúmeros planos quando nós sabemos exatamente o que é melhor para você? Assine o nosso plano mensal Pro Premium VIP e garanta mensalmente um ebook novo de programação. E por menos de um café por dia. ">
+          <CardComponent />
+        </CallOutComponent>
+      </section>
+
+      <section className="bg-white px-4 md:px-0">
+        <CallOutComponent
+            title="Pronto Para Mudar Sua Vida?" 
+            description="Faça como milhares de outras pessoas. Assine nosso produto e tenha garantido seus estudos ">
+
+            <div className="description flex flex-col items-center justify-center gap-6 min-w-full lg:min-w-[422px]">
+                <Button className="w-full">  
+                  Assine Agora
+                </Button>
+                <span className="text-[#6B7280] text-sm font-light"> Comece sua assinatura agora mesmo. Cancele quando quiser.  </span>
+            </div>
+
+          
+        </CallOutComponent>
+      </section>
+
+
+      <section className="bg-white px-4 md:px-0">
+        <FooterComponent />
       </section>
 
     </main>
